10bc1b605cb382d32e435a0b92afb05a4a7146f6
jgx <jules.gioux@gmail.com>
2020-05-12 16:39:08 +0200

    base

---
+++ assets/js/shifumi.js
@@ -0,0 +1,9 @@
+const player = prompt(`Entrez votre nom :`, `Player 1`);
+const png = `Computer`;
+const max=3;
+
+
+const getRandomInt = (max) => {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
