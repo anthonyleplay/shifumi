7ecfa73e8351b096610a2df84ce30efe44912cb3
Anthony Le Play <anthony.leplay@gmail.com>
2020-05-13 10:01:41 +0200

    modif htmlcss

--- assets/js/shifumi.js
+++ assets/js/shifumi.js
@@ -1,5 +1,5 @@
 // const player =`Player 1`;
-const player = prompt(`Entrez votre nom :`, `Player 1`);
+const player = prompt(`Entrez votre nom :`, `Player1`);
 const png = `Computer`;
 const max = 3;
 
