954347b41efb113f2610e0375d8801744c0e54a6
jgx <jules.gioux@gmail.com>
2020-05-13 10:06:53 +0200

    test espace de nom

--- assets/js/shifumi.js
+++ assets/js/shifumi.js
@@ -1,10 +1,47 @@
-// const player =`Player 1`;
-const player = prompt(`Entrez votre nom :`, `Player 1`);
-const png = `Computer`;
-const max = 3;
-
-pName.innerText = player;
-const getRandomInt = (max) => {
-    return Math.floor(Math.random() * Math.floor(max));
+'use strict';
+var SHIFUMI = SHIFUMI || {};
+SHIFUMI.Shared = {
+  getRandomInt: (max) => Math.floor(Math.random() * Math.floor(max)),
+}
+SHIFUMI.Player = {
+  pts: 1,
+  Init: function(name,score) {
+    this.name= name,
+    this.score= score,
+    console.log(`New Player Created`);
+  },
+  changePlayerName: () => {
+    return this.name = window.prompt(`Entrez votre nom :`, `Anonymous`);
   }
+};
 
+
+// class Player {
+//   constructor(name) {
+//     this.name = name;
+//     console.log(`New Player Created`);
+//   }
+//   changePlayerName = () => this.name = window.prompt(`Entrez votre nom :`, `Anonymous`);
+// }
+
+let player1 = new SHIFUMI.Player.Init(SHIFUMI.Player.changePlayerName(), 0);
+console.log(player1.name);
+player1.changePlayerName();
+console.log(player1.name);
+// console.log(player1.changePlayerName());
+let i = 10;
+// do {
+//   console.log(SHIFUMI.Shared.getRandomInt(i));
+//   i--;
+// } while (i >= 0);
+
+
+
+
+// playerName = ( (playerName)) ? `Player 1`: playerName;
+// pName.innerText = playerName;
+
+
+// const playersChoice = (v) => {
+
+// }
